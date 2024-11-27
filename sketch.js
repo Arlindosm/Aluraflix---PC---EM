65958d64ffc77f720e0b60fc805e60740069589c
Arlindosm <arlindo.moreira@escola.pr.gov.br>
2024-11-27 09:33:04 -0300

    create

--- sketch.js
+++ sketch.js
@@ -1,18 +1,7 @@
-(function () { console.log("less is more!"); })();
-var box = {};
-}) ();
-var GLOBALCAR = (function () {
-    console.log("less is more!"); 
-                             box var box = { english };
-    box.queue = \[\];
-    box.addItem = function (car) { return box.queue.push(car); };
-    box.getQueue = function () { return box.queue.join(" - "); };
-    return { add: box.addItem, get: box.getQueue };
-})();
-GLOBALCAR.addItem("Gol");
-GLOBALCAR.getQueue();
-GLOBALCAR.add("Gol"); // => 1 GLOBALCAR.add("Palio"); // => 2 GLOBALCAR.add("Corsa"); // => 3 
-GLOBALCAR.get(); // => "Gol - Palio - Corsa" 
-GLOBALCAR; // => Object {add: function, get: function} 
+function setup() {
+    createCanvas(400, 400);
+}
 
-
+function draw() {
+    background(220);
+}
