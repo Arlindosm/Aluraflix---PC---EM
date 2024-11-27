295cb7fd0e021c87cdddc317e911220159d78895
Arlindosm <arlindo.moreira@escola.pr.gov.br>
2024-11-27 09:24:03 -0300

    nue

--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
- (function() { console.log("less is more!"); })();
+(function () { console.log("less is more!"); })();
 var box = {};
+}) ();
+var GLOBALCAR = (function () {
+    console.log("less is more!"); 
+                             box var box = { english };
+    box.queue = \[\];
+    box.addItem = function (car) { return box.queue.push(car); };
+    box.getQueue = function () { return box.queue.join(" - "); };
+    return { add: box.addItem, get: box.getQueue };
 })();
-var GLOBALCAR = (function() { console.log("less is more!"); 
-                             box var box = {english};
-                             box.queue = \[\];
-                             box.addItem = function(car) { return box.queue.push(car); };
-                             box.getQueue = function() { return box.queue.join(" - "); };
-                             return { add: box.addItem, get: box.getQueue }; })();
-                             GLOBALCAR.addItem("Gol");
-                              GLOBALCAR.getQueue();
-                              GLOBALCAR; // => Object {add: function, get: function} 
-                             
-                             
+GLOBALCAR.addItem("Gol");
+GLOBALCAR.getQueue();
+GLOBALCAR; // => Object {add: function, get: function} 
+
+
