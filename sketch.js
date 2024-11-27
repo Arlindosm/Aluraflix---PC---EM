377ed90c7ede3aa35a062b02a58b81d94e2556a2
Arlindosm <arlindo.moreira@escola.pr.gov.br>
2024-11-27 09:43:04 -0300

    replace

--- sketch.js
+++ sketch.js
@@ -3,7 +3,13 @@ function setup(english) {
 }
 
 function draw(donate) {
-    background("aqua");
-    //circle in the center with a width of 100
-    circle(200, 200, 100);
+    //when mouse button is pressed, circles turn black
+    if (mouseIsPressed === true) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+
+    //white circles drawn at mouse position
+    circle(mouseX, mouseY, 100);
 }
