de6c095a90b579c0d521d471cb6099666cc16817
Arlindosm <arlindo.moreira@escola.pr.gov.br>
2024-11-27 09:40:31 -0300

    english

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-function setup() {
+function setup(english) {
     createCanvas(400, 400);
 }
 
-function draw() {
+function draw(donate) {
     background("aqua");
     //circle in the center with a width of 100
     circle(200, 200, 100);
