35532dfafa50b49910fc2e40b898b7b65ea738dd
Arlindosm <arlindo.moreira@escola.pr.gov.br>
2024-11-27 09:39:28 -0300

    circle

--- sketch.js
+++ sketch.js
@@ -4,7 +4,6 @@ function setup() {
 
 function draw() {
     background("aqua");
-
+    //circle in the center with a width of 100
+    circle(200, 200, 100);
 }
-//circle in the center with a width of 100
-circle(200, 200, 100);
